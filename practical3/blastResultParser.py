cdbfb6455d61c96f1f77e33669e156dbf3536bee
maxsenh <maximilian.senftleben@googlemail.com>
2018-05-08 14:29:47 +0200

    pract3 continue

--- practical3/blastResultParser.py
+++ practical3/blastResultParser.py
@@ -14,7 +14,7 @@ blastOutputXMLFile = sys.argv [1]
 blastOutputXMLHandle = open (blastOutputXMLFile)
 
 listOfBlastRecords = NCBIXML.parse (blastOutputXMLHandle)
-
+outputfile=open("out"+str(blastOutputXMLFile),"w")
 for aSingleBlastRecord in listOfBlastRecords:
 
 	for i in range (len (aSingleBlastRecord.alignments)):
@@ -22,6 +22,9 @@ for aSingleBlastRecord in listOfBlastRecords:
 		description = aSingleBlastRecord.descriptions [i]
 		alignment = aSingleBlastRecord.alignments [i]
 		title = re.compile ("gnl\|BL_ORD_ID\|\d* ").sub ("", description.title)
-	
-		print ">" + title	
-		print alignment.hsps [0].sbjct
+		
+		print (">" + title	)
+		print (alignment.hsps [0].sbjct)
+		outputfile.write(">" + title)
+		outputfile.write(alignment.hsps [0].sbjct)
+outputfile.close()
