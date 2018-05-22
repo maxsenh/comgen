5d2a95e1e9fa531efafbbc207106ef86c6bd365f
maxsenh <maximilian.senftleben@googlemail.com>
2018-05-22 15:38:04 +0200

    pract8

--- practical8/quest1.py
+++ practical8/quest1.py
@@ -4,28 +4,14 @@ import gzip
 
 
 
-def extracting(path):
-	names=["scerev","lgelid","nmenin","rbalti","tmarit"]
-	ids=["4932","927691","122586","243090","243274"]
-	db=dict(zip(ids,names))
-	print(str(ids[0]))
-	scerev=[]
-	lgelid=[]
-	nmenin=[]
-	rbalti=[]
-	tmarit=[]
-	with gzip.open(path+"protein.links.v10.5.txt.gz","r") as f:
-		for line in f:
-			if line.startswith(b"4932"):
-				scerev.append(line)
-			if line.startswith(b"927691"):
-				lgelid.append(line)
-			if line.startswith(b"122586"):
-				nmenin.append(line)
-			if line.startswith(b"243090"):
-				rbalti.append(line)
-			if line.startswith(b"243274"):
-				tmarit.append(line)
-	
-extracting("../../protlinks/")
-			
+def calculate_average(path):
+	for i in range(1,6):
+		op=open(path+"file"+str(i)+".txt")
+		cnt=0
+		s=set()		
+		for line in op:
+			gen_a,gen_b,score=line.split()
+			s.add(gen_a)
+			cnt=cnt+1
+		print("File: %s, Number of Genes: %d, Number of Interactions: %d, Average connectivity: %.4f"%(i,len(s),cnt,cnt/len(s)))
+calculate_average("./quest1/")
