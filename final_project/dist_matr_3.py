c0e084ea643db0779f4276f87aff335b2bce35e7
maxsenh <maximilian.senftleben@googlemail.com>
2018-05-26 03:18:57 +0200

    some work because theres a party a floor below me

--- final_project/dist_matr_3.py
+++ final_project/dist_matr_3.py
@@ -6,3 +6,29 @@ Created on Fri May 25 16:47:28 2018
 """
 
 #3. Distance matrix tool
+import matplotlib
+matplotlib.use("Agg")
+from stat_tool_1 import nucl_freq
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+def dist(file1,file2):
+    db_static1,gc1=nucl_freq(file1)
+    db_static2,gc2=nucl_freq(file2)
+    print(gc1,gc2)
+    print(db_static1)
+    print(db_static2)
+    #3
+def plot_matrix(seq_0, seq_1):
+    mat=np.zeros(shape=(len(seq_0),len(seq_1)))
+    for x in range(0,len(seq_0)):
+        for y in range(0,len(seq_1)):
+            if seq_0[x]==seq_1[y]:
+                mat[x,y]=1
+    print(mat)
+    #plt.matshow(mat)
+    #plt.savefig("out.png")
+
+plot_matrix("ABC","ABX")    
+#dist(sys.argv[1],sys.argv[2])
--- final_project/stat_tool_1.py
+++ final_project/stat_tool_1.py
@@ -20,42 +20,20 @@ def gc_content(file):
 			seq=line
 	op.close()
 	gc=(seq.count("C")+seq.count("G"))/len(seq)
-	print("genome: %s"%(title[3::]))
+	#print("genome: %s"%(title[3::]))
 	#print("GC-content: %.4f"%(gc))
 	return gc,seq
 	
 def nucl_freq(file):
-	gc,seq=gc_content(file)
-	sett=["A","C","G","T","N","AA","AC","AT","AG","CA","CC","CT","CG","TA","TC","TT","TG","GA","GC","GT","GG"]
-	print("GC-content: %.4f"%(gc))
-	for nucl in sett:		
-		print("#%s = %d/%d"%(nucl,seq.count("A"),len(seq)))
-	'''
-	print("#C = %d/%d"%(seq.count("C"),len(seq)))
-	print("#T = %d/%d"%(seq.count("T"),len(seq)))
-	print("#G = %d/%d"%(seq.count("G"),len(seq)))
-	print("#N = %d/%d"%(seq.count("N"),len(seq)))
-
-	print("#AA = %d/%d"%(seq.count("AA"),len(seq)))
-	print("#AC = %d/%d"%(seq.count("AC"),len(seq)))
-	print("#AT = %d/%d"%(seq.count("AT"),len(seq)))
-	print("#AG = %d/%d"%(seq.count("AG"),len(seq)))
+    gc,seq=gc_content(file)
+    sett=["A","C","G","T","N","AA","AC","AT","AG","CA","CC","CT","CG","TA","TC","TT","TG","GA","GC","GT","GG"]
+    print("GC-content: %.4f"%(gc))
+    db_static={}
+    for nucl in sett:		
+        print("#%s = %d/%d"%(nucl,seq.count(nucl),len(seq)))
+        db_static[nucl]=seq.count(nucl)/len(seq)
+    return db_static,gc
 	
-	print("#CA = %d/%d"%(seq.count("CA"),len(seq)))
-	print("#CC = %d/%d"%(seq.count("CC"),len(seq)))
-	print("#CT = %d/%d"%(seq.count("CT"),len(seq)))
-	print("#CG = %d/%d"%(seq.count("CG"),len(seq)))
-	
-	print("#TA = %d/%d"%(seq.count("TA"),len(seq)))
-	print("#TC = %d/%d"%(seq.count("TC"),len(seq)))
-	print("#TT = %d/%d"%(seq.count("TT"),len(seq)))
-	print("#TG = %d/%d"%(seq.count("TG"),len(seq)))
-	
-	print("#GA = %d/%d"%(seq.count("GA"),len(seq)))
-	print("#GC = %d/%d"%(seq.count("GC"),len(seq)))
-	print("#GT = %d/%d"%(seq.count("GT"),len(seq)))
-	print("#GG = %d/%d"%(seq.count("GG"),len(seq)))
-	'''
 def aminoacid_freq(file):
 	op=open("./proteomes/"+file)
 	sett="ACDEFGHIKLMNPQRSTVWY"
@@ -70,5 +48,5 @@ def aminoacid_freq(file):
 		print("#%s = %d/%d"%(amino,seq.count(str(amino)),len(seq)))
 	
 #gc_content(sys.argv[1])
-nucl_freq(sys.argv[1])
+#nucl_freq(sys.argv[1])
 #aminoacid_freq(sys.argv[1])
