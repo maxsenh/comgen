4c3317d272a81559155a576553dd31666a44c6e9
maxsenh <maximilian.senftleben@googlemail.com>
2018-05-28 17:20:18 +0200

    daryl bought a new bike

--- final_project/dist_matr_3.py
+++ final_project/dist_matr_3.py
@@ -27,10 +27,15 @@ def dist(list_of_in):
     mat_GC=np.zeros(shape=(len(gc_list),len(gc_list)))
     for x in range(0,len(gc_list)):
         for y in range(0,len(gc_list)):
-            mat_GC[x,y]=math.sqrt((gc_list[x]-gc_list[y])**2)
+            mat_GC[x,y]=abs(gc_list[x]-gc_list[y])
     print([i for i in list_of_in])									
     print(mat_GC)
+    #np.savetxt("gc_content_dist_matrix.grimm",np.asarray([float(i) for i in list_of_in]))
     np.savetxt("gc_content_dist_matrix.grimm",mat_GC)
+    with open("gc_content_dist_matrix.grimm", "a") as myfile:
+        myfile.write("16 20 29 44 47")
+
+
     '''
     #Nucleotide frequency
     mat_GC=np.zeros(shape=(len(gc_list),len(gc_list)))
@@ -40,6 +45,6 @@ def dist(list_of_in):
     print([i for i in list_of_in])									
     print(mat_GC)
     '''
-				
+
 #plot_matrix("AB","AB")    
 dist(["16","20","29","44","47"])
